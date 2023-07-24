6b9fb4818a67c794e0d15788ed6174add10c3a44
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2023-07-24 23:58:12 +0000

    Do not output error message for missing wavelengh/channel unless the optional rc argument is provided.

--- Process_Library/GOCART2G_MieMod.F90
+++ Process_Library/GOCART2G_MieMod.F90
@@ -1,3 +1,4 @@
+#include "Process.H"
 !BOP
 !
 ! !MODULE:  GOCART2G_MieMod --- Reader for aerosol mie tables
@@ -530,13 +531,15 @@ CONTAINS
        endif
     enddo
 
-    if (present(rc)) rc = 0
-
-    if (ch < 0) then
-       !$omp critical
-       print*, "wavelength of ",wavelength, " is an invalid value."
-       !$omp end critical
-       if (present(rc)) rc = -1
+    if (present(rc)) then
+       if (ch > 0) then
+          rc = __SUCCESS__
+       else
+          rc = __FAIL__
+          !$omp critical
+          print*, "wavelength of ",wavelength, " is an invalid value."
+          !$omp end critical
+       endif
     endif
 
   end function getChannel
@@ -548,18 +551,22 @@ CONTAINS
      real, parameter :: w_tol = 1.e-9
      integer :: i
 
-     if (present(rc)) rc = 0
-
      if (ith_channel <=0 .or. ith_channel > this%nch ) then
-       !$omp critical
-       print*, "The channel of ",ith_channel, " is an invalid channel number."
-       !$omp end critical
-       if (present(rc)) rc = -1
-       wavelength = -1. ! meanlingless nagative
-       return
+        wavelength = -1. ! meaningless negative
+     else
+        wavelength = this%wavelengths(ith_channel)
      endif
-     
-     wavelength = this%wavelengths(ith_channel)
+
+    if (present(rc)) then
+       if (wavelength > 0) then
+          rc = __SUCCESS__
+       else
+          rc = __FAIL__
+          !$omp critical
+          print*, "The channel of ",ith_channel, " is an invalid channel number."
+          !$omp end critical
+       endif
+    endif
 
   end function getWavelength
 
