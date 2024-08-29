a984ba9b726643c92a6c8cfd6a615af9ce501888
Barry Baker <barry.baker@noaa.gov>
2024-08-29 09:05:13 -0400

    fix the self%fwet* variables

--- ESMF/GOCART2G_GridComp/GA_Environment/GA_EnvironmentMod.F90
+++ ESMF/GOCART2G_GridComp/GA_Environment/GA_EnvironmentMod.F90
@@ -73,9 +73,9 @@ module GA_EnvironmentMod
        call ESMF_ConfigGetAttribute (cfg, self%fnum,       label='fnum:', __RC__)
        call ESMF_ConfigGetAttribute (cfg, self%rhFlag,     label='rhFlag:', __RC__)
        call ESMF_ConfigGetAttribute (cfg, self%plid,       label='pressure_lid_in_hPa:', __RC__)
-       call ESMF_ConfigGetAttribute (cfg, self%fwet,       label='fwet_ice:', __RC__)
-       call ESMF_ConfigGetAttribute (cfg, self%fwet,       label='fwet_snow:', __RC__)
-       call ESMF_ConfigGetAttribute (cfg, self%fwet,       label='fwet_rain:', __RC__)
+       call ESMF_ConfigGetAttribute (cfg, self%fwet_ice,   label='fwet_ice:', __RC__)
+       call ESMF_ConfigGetAttribute (cfg, self%fwet_snow,  label='fwet_snow:', __RC__)
+       call ESMF_ConfigGetAttribute (cfg, self%fwet_rain,  label='fwet_rain:', __RC__)
        call ESMF_ConfigGetAttribute (cfg, wet_removal_scheme, label='wet_removal_scheme:', default='gocart', __RC__)
        self%wet_removal_scheme = ESMF_UtilStringLowerCase(trim(wet_removal_scheme), __RC__)
 
